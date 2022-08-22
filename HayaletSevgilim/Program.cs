050e558ceb5471944b43115bb920bd9865b62a9f
atakanak <atakanaak@gmail.com>
2022-08-22 11:57:07 +0300

    wip

---
+++ HayaletSevgilim/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var lyrics = new HayaletSevgilim.HayaletSevgilim();
+
+foreach (var line in lyrics.Lines)
+{
+    Thread.Sleep(500); //ms
+    Console.WriteLine(line);
+}
